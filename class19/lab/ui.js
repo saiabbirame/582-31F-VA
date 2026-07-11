0099b1a361bfeb345c5689f528332b9d05277411
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-07-10 23:41:12 -0400

    Create rendering module

---
+++ class19/lab/ui.js
@@ -0,0 +1,36 @@
+export function renderArtists(artists, container) {
+    container.innerHTML = "";
+
+    for (let i = 0; i < artists.length; i++) {
+        const artistCard = document.createElement("artist-card");
+
+        artistCard.setAttribute("id", artists[i].id);
+        artistCard.setAttribute("name", artists[i].name);
+        artistCard.setAttribute("genre", artists[i].genre);
+        artistCard.setAttribute("stage", artists[i].stage);
+        artistCard.setAttribute("time", artists[i].time);
+        artistCard.setAttribute("country", artists[i].country);
+        artistCard.setAttribute("headliner", artists[i].headliner);
+
+        container.appendChild(artistCard);
+    }
+}
+
+export function clearDashboard(lineupContainer, detailsContainer) {
+    lineupContainer.innerHTML = "";
+
+    detailsContainer.innerHTML = `
+        <p>No artist selected yet.<p>
+    `;
+}
+
+export function renderArtistDetails(artist, detailsContainer) {
+    detailsContainer.innerHTML = `
+        <h3>${artist.name}</h3>
+        <p>Genre: ${artist.genre}</p>
+        <p>Stage: ${artist.stage}</p>
+        <p>Time: ${artist.time}</p>
+        <p>Country: ${artist.country}</p>
+        <p>Headliner: ${artist.headliner ? "Yes" : "No"}</p>
+    `;
+}
